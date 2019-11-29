3d6b253ab40a7f3557133422957362dd0a97731e
LESANF <54767632+LESANF@users.noreply.github.com>
2019-11-29 17:03:37 +0900

    ING

--- app.js
+++ app.js
@@ -22,13 +22,12 @@ function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
   if (!painting) {
-    console.log(x, y);
-    ctx.beginPath();
-    ctx.moveTo(x, y);
+    // painting이 false일 경우 if문 실행 true면 else문 실행
+    ctx.beginPath(); //경로를 만들기위한 첫번째 단계
+    ctx.moveTo(x, y); // 펜을 x, y로 지정된 좌표로 옮김.
   } else {
-    console.log(x, y);
-    ctx.lineTo(x, y);
-    ctx.stroke();
+    ctx.lineTo(x, y); // 현재 드로잉위치에서 x,y로 지정된 위치까지 선을 그림.
+    ctx.stroke(); //윤곽선을 이용하여 도형을 그림.
   }
 }
 
@@ -38,8 +37,9 @@ function onMouseDown(event) {
 }
 
 if (canvas) {
-  canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", startPainting);
-  canvas.addEventListener("mouseup", stopPainting);
-  canvas.addEventListener("mouseleave", stopPainting);
+  // canvas가 있을때 아래의 event실행
+  canvas.addEventListener("mousemove", onMouseMove); // 캔버스위에 마우스를 올렸을 때, onMouseMove함수 실행
+  canvas.addEventListener("mousedown", startPainting); // 캔버스위에 마우스를 눌렀을 때, startPainting함수 실행
+  canvas.addEventListener("mouseup", stopPainting); // 캔버스위에서 마우스를 눌렀다 땠을때, stopPainting함수 실행
+  canvas.addEventListener("mouseleave", stopPainting); // 캔버스에서 마우스커서가 벗어 났을때, stopPainting함수 실행
 }
